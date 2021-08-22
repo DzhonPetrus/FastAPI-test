8efa4ca3998cfe2cbced01ac6eb842bdb95d8c23
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-08-22 17:15:54 +0800

    jinja template added, user templates added

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from . import models, routers
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(engine)
+
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.mount("/static", StaticFiles(directory="blog/static"), name="static")
+
 
 app.include_router(routers.authentication.router)
 app.include_router(routers.blog.router)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,11 +1,18 @@
 
 from typing import List
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request
 from sqlalchemy.orm import Session
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from .. import schemas, database
 
 from ..controllers import user
+
+
+
+templates = Jinja2Templates(directory="blog/templates")
 
 router = APIRouter(
     prefix="/user",
@@ -20,9 +27,11 @@ def create(User: schemas.User, db: Session = Depends(get_db)):
     return user.create(User, db)
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
-def show(id, db: Session = Depends(get_db)):
-    return user.get_one(id, db)
+def show(request: Request, id, db: Session = Depends(get_db)):
+    # return user.get_one(id, db)
+    return templates.TemplateResponse("user.html", {"request":request, "user": user.get_one(id, db)})
 
-@router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowUser])
-def all(db: Session = Depends(get_db)):
-    return user.get_all(db)
+@router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowUser], response_class=HTMLResponse)
+def all(request: Request, db: Session = Depends(get_db)):
+    # return user.get_all(db)
+    return templates.TemplateResponse("users.html", {"request":request, "users": user.get_all(db)})
